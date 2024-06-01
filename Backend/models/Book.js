322522d5cc665546cd4ccbb3bd3844a1c6983051
Carlos Barone <128315634+carbarcor@users.noreply.github.com>
2024-06-01 18:11:34 +0200

    Databasmodell fix

--- Backend/models/Book.js
+++
@@ -1,9 +0,0 @@
-// models/Book.js
-const mongoose = require('mongoose');
-
-const bookSchema = new mongoose.Schema({
-  isbn: { type: String, required: true, unique: true },
-  title: { type: String },
-});
-
-module.exports = mongoose.model('Book', bookSchema);
